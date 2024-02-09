ce419039974e8a19c500c781031db23c68b93ed0
Normunds Lazdins <normundslazdins16@gmail.com>
2024-02-09 15:51:26 +0200

    suggestions work

--- larstadi/app/Http/Controllers/StadiController.php
+++ larstadi/app/Http/Controllers/StadiController.php
@@ -12,16 +12,19 @@ class StadiController extends Controller
         $limit = $request->input('limit', 10);
         $offset = $request->input('offset', 0);
         $search = $request->input('search', '');
-    
+        
         $query = Stadi::query();
-    
+        
         if ($search) {
             $query->where('base_name', 'like', '%' . $search . '%');
         }
-    
+        
         $stadi = $query->skip($offset)->take($limit)->get();
     
-        return response()->json(['stadi' => $stadi], 200);
+        // Assuming suggestions are based on the search query
+        $suggestions = Stadi::where('base_name', 'like', '%' . $search . '%')->limit(5)->get(['base_name']);
+    
+        return response()->json(['stadi' => $stadi, 'suggestions' => $suggestions], 200);
     }
     public function show($base_name)
     {
