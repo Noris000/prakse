9341d8bb63269ef0022666559e5cf8b4f997f712
Normunds Lazdins <normundslazdins16@gmail.com>
2024-02-09 16:44:57 +0200

    fixed up the code

--- larstadi/app/Http/Controllers/StadiController.php
+++ larstadi/app/Http/Controllers/StadiController.php
@@ -21,17 +21,14 @@ class StadiController extends Controller
         
         $stadi = $query->skip($offset)->take($limit)->get();
     
-        // Assuming suggestions are based on the search query
         $suggestions = Stadi::where('base_name', 'like', '%' . $search . '%')->limit(5)->get(['base_name']);
     
         return response()->json(['stadi' => $stadi, 'suggestions' => $suggestions], 200);
     }
     public function show($base_name)
     {
-        // Find a specific Stadi record by base_name
         $stadi = Stadi::where('base_name', $base_name)->firstOrFail();
 
-        // Return the view with the JSON data
         return view('stadi.show', ['stadi' => $stadi]);
     }
 }
